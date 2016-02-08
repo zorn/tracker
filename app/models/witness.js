85e9d08ad52df744e87c067c5577ca119e3c31aa
Mike Zornek <mike@clickablebliss.com>
2016-02-08 16:34:18 -0500

    Work from Model chapter.

---
+++ app/models/witness.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    fName: DS.attr('string'),
+    lName: DS.attr('string'),
+    email: DS.attr('string'),
+    sightings: DS.hasMany('sighting'),
+    // computed property
+    fullName: Ember.computed('fName', 'lName', function(){
+      return this.get('fName') + ' ' + this.get('lName');
+    })
+});
